1b4ff2a72dfffa4b782208efdb6eea74c8c2a333
HesterG <hestergong@gmail.com>
2023-07-04 12:58:19 +0800

    update tags

--- routers/web/web.go
+++ routers/web/web.go
@@ -1370,6 +1370,7 @@ func registerRoutes(m *web.Route) {
 		m.Group("/{reponame}", func() {
 			m.Get("", repo.SetEditorconfigIfExists, repo.Home)
 			m.Get("/branches", repo.GetBranches)
+			m.Get("/tags", repo.GetTags)
 		}, ignSignIn, context.RepoAssignment, context.RepoRef(), context.UnitTypes())
 
 		m.Group("/{reponame}", func() {
